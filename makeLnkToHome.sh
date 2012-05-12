1070ecb73f626f380d2ecad89cf295d18f406c6e
tanakayuki <a0673040@gmail.com>
2012-05-12 17:20:35 +0900

    add make lnk script

---
+++ makeLnkToHome.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd $(dirname $0)
+
+# copy dotsfile to home dir
+for homefile in `ls -a`
+do
+    if [ $homefile != '..' ] && 
+       [ $homefile != '.' ] && 
+       [ "$homefile" != "`basename $0`" ] && 
+       [ $homefile != '.git' ] && 
+       [ $homefile != '.gitignore' ]
+    then
+        ln -Fis "$PWD/$homefile" $HOME
+    fi
+done
+
