93bc77a7b380a0ebeb063969bfd2f77f89eaf51b
tanakayuki <a0673040@gmail.com>
2012-12-02 11:34:00 +0900

    add neco-ruby-keyword and custom neosnippet

---
+++ .vim/snippets/test.js
@@ -0,0 +1,27 @@
+(function() {
+    
+})();
+
+document.
+
+
+
+
+
+/**
+ * test
+ */
+var SampleClass = function() {
+    this.init.apply(this, arguments);
+};
+
+SampleClass.prototype = {
+
+    /**
+     * Constructor
+     */
+    init: function(type, name) {
+              this.type = type;
+              this.name = name;
+          }
+};
